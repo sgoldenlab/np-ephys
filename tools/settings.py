e322b846bd8f576d266dc310987fb25a1d2b45ae
Kevin N. Schneider <kevsch88@gmail.com>
2025-09-19 12:29:51 -0700

    refactor: adding override from .env file if present

--- tools/settings.py
+++ tools/settings.py
@@ -1,8 +1,12 @@
+import os
 import pathlib
 from pydantic_settings import BaseSettings, PydanticBaseSettingsSource, SettingsConfigDict, TomlConfigSettingsSource
 from pydantic import BaseModel
 from typing import Tuple, Type
+from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv("config/.env"))
+run_config_file = os.getenv('RUN_CONFIG_FILE')
 
 class PathsConfig(BaseModel):
     drive: pathlib.Path
@@ -29,7 +33,7 @@ class Settings(BaseSettings):
     experiment: ExperimentConfig
 
     model_config = SettingsConfigDict(
-        toml_file = ["config/default_config.toml", "config/run_config.toml"],
+        toml_file = ["config/default_config.toml", run_config_file if run_config_file else "config/run_config.toml"],
         env_prefix='PIPELINE_',
         env_nested_delimiter='__',
         env_file='config/.env',
